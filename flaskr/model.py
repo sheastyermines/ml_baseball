b66d90fcc21fc121d8efc3d2497c4f5d141f0e41
sheastyermines <sheastyer@mines.edu>
2018-12-06 14:06:11 -0700

    added folders

---
+++ flaskr/model.py
@@ -0,0 +1,6 @@
+from saved_models import model_1
+
+def run_model(team, year, model="sameYear"):
+    return 42
+
+    #load("saved_models/XXX.h5")
--- flaskr/views.py
+++ flaskr/views.py
@@ -4,6 +4,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
+from model import run_model
 
 bp = Blueprint('bp', __name__, url_prefix='/app')
 
@@ -11,10 +12,25 @@ bp = Blueprint('bp', __name__, url_prefix='/app')
 def home_page():
     return render_template('home.html')
 
-@bp.route('/one_year')
+@bp.route('/one_year', methods=('GET', 'POST'))
 def one_year_page():
-    return render_template('one_year.html')
+    if request.method == 'POST':
+        flash("POSTED")
+        # team_name: String
+        # year: Int
+        ## wins = FUNC CALL HERE 
+        return render_template('one_year.html')
+    else:
+        return render_template('one_year.html')
 
-@bp.route('/five_year')
+@bp.route('/five_year', methods=('GET', 'POST'))
 def five_year_page():
-    return render_template('five_year.html')
+    if request.method == 'POST':
+        flash("POSTED")
+        # team_name: String
+        # year: Int
+        # num_years: Int
+        ## FUNC CALL HERE
+        return render_template('five_year.html')
+    else:
+        return render_template('five_year.html')
